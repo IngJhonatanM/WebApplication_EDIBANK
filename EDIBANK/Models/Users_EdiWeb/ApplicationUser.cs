efc48a91f3cb4bb5eda0964914c6790bb07afec2
IngJhonatanM <jhonatan.m20233@gmail.com>
2025-01-17 09:51:40 -0400

    Add project files.

---
+++ EDIBANK/Models/Users_EdiWeb/ApplicationUser.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace EDIBANK.Models.Users_EdiWeb
+{
+    public class ApplicationUser : IdentityUser
+    {
+        public string? DescripUser { get; set; }
+
+        public string? EDIId { get; set; }
+    }
+}
